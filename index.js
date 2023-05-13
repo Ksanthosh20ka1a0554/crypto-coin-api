e594f75acf4893d6ef363a0c65449e6798ae6716
K Santhosh <87223844+Ksanthosh20ka1a0554@users.noreply.github.com>
2023-05-13 21:54:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,38 @@
 const express = require('express');
 const https = require('https');
-var mysql = require('mysql');
-
+const mysql = require('mysql');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-var result_data = [];
-
-app.get('/', function (req, res) {
-
-    const url = 'https://api.wazirx.com/api/v2/tickers';
-
-    https.get(url, (response) => {
-        let data = '';
-        response.on('data', (chunk) => {
-            data += chunk;
-        });
-        response.on('end', () => {
-            const apiData = JSON.parse(data);
-            var result = [];
-            for (var i = 0; i < 11; i++) {
-                result.push(Object.keys(apiData)[i]);
-            }
-            
-             for (var i = 0; i < 11; i++) {
-                    var index = result[i];
-                    var name_value = apiData[index].name;
-                    var last_value = apiData[index].last;
-                    var buy_value = apiData[index].buy;
-                    var sell_value = apiData[index].sell;
-                    var volume_value = apiData[index].volume;
-                    var base_unit_value = apiData[index].base_unit;
-                    var data = {
-                            title: apiData[index].name,
-                            last_item: apiData[index].last,
-                            buy_item: apiData[index].buy,
-                            sell_item: apiData[index].sell,
-                            volume_item: apiData[index].volume,
-                            base_unit_item: apiData[index].base_unit,
-                        }
-                        result_data.push(data);
-                        
-                }
-                res.render('list',{bitcoin_data:result_data});
-            
-        });
-        
-    });
+app.get('/', async (req, res) => {
+  const url = 'https://api.wazirx.com/api/v2/tickers';
+  
+  try {
+    const apiResponse = await fetch(url);
+    const apiData = await apiResponse.json();
     
-    
-
+    const resultData = Object.entries(apiData)
+      .slice(0, 11)
+      .map(([key, value]) => ({
+        title: value.name,
+        last_item: value.last,
+        buy_item: value.buy,
+        sell_item: value.sell,
+        volume_item: value.volume,
+        base_unit_item: value.base_unit
+      }));
+      
+    res.render('list', { bitcoin_data: resultData });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
 });
 
-
-
-
-
-
-
-
-
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
